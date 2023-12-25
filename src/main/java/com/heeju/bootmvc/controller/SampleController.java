951fdfbccb9a7f2b491cfe5a7d5e341e4bab2538
zakard114 <infantry_hooa@hotmail.com>
2023-12-25 10:24:55 +1000

    Update Note #2
    - When Thymeleaf uses data imported from a model, an error may occur in the form of ‘the corresponding variable cannot be found.’ The solution is IntelliJ's settings -> Search for 'Thymeleaf' -> Uncheck "Unresolved references...".
    If you close the existing HTML editor after changing the settings and open it again, you can see that the variable is not checked (the variable no longer has an underline).
    
    - Add the following DI to build.gradle for layout functionality: "implementation 'nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.1.0'"
    
    - Create a layout folder and create layout1.html for the layout, and add the following:
    <html xmlns:layout="http://www.ultraq.net.nz/thymeleaf/layout"
           xmlns:th="http://www.thymeleaf.org" lang="en">
    
    - Add the following to implement ex3.html in layout:fragment=content of layout/layout1.html:
    <html xmlns:th="http://www.thymeleaf.org"
       xmlns:layout="http://www.ultraq.net.nz/thymeleaf/layout"
           layout:decorate="~{layout/layout1.html}" lang="en">

--- src/main/java/com/heeju/bootmvc/controller/SampleController.java
+++ src/main/java/com/heeju/bootmvc/controller/SampleController.java
@@ -1,12 +1,20 @@
 package com.heeju.bootmvc.controller;
 
+import lombok.Data;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -20,4 +28,45 @@ public class SampleController {
         return new ModelAndView("hello", "msg", msg);
     }
 
+    @GetMapping("/ex/ex1")
+    public ModelAndView ex1(){
+
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+
+        return new ModelAndView("ex/ex1", "list", list);
+    }
+
+    @Getter
+    class SampleDTO {
+        private String p1, p2, p3;
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model){
+        log.info("ex/ex2...............");
+
+        List<String> strList = IntStream.range(1,10)
+                .mapToObj(i->"Data"+i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+
+        model.addAttribute("dto", sampleDTO);
+    }
+
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model){
+        model.addAttribute("arr", new String[]{"AAA","BBB","CCC"});
+    }
 }
